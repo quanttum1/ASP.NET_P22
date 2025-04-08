97a05b8ff73a60b3089215473f0aa715a611c222
novakvova <novakvova@gmail>
2025-04-08 20:46:49 +0300

    Simple MVC Project

---
+++ WebSmonder/WebSmonder/Models/Category/CategoryItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebSmonder.Models.Category
+{
+    public class CategoryItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public string Image { get; set; } = String.Empty;
+    }
+}
---
+++ WebSmonder/WebSmonder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSmonder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
