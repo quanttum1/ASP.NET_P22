98ebed810c76fc516880b19800f976e80716aa12
novakvova <novakvova@gmail>
2025-05-22 19:56:41 +0300

    Додали валдіацію для назви

--- WebApiPizushi/WebApiPizushi/Validators/Category/CategoryCreateValidator.cs
+++ WebApiPizushi/WebApiPizushi/Validators/Category/CategoryCreateValidator.cs
@@ -1,15 +1,20 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using WebApiPizushi.Data;
 using WebApiPizushi.Models.Category;
 
 namespace WebApiPizushi.Validators.Category;
 
 public class CategoryCreateValidator : AbstractValidator<CategoryCreateModel>
 {
-    public CategoryCreateValidator()
+    public CategoryCreateValidator(AppDbPizushiContext db)
     {
         RuleFor(x => x.Name)
             .NotEmpty()
             .WithMessage("Назва є обов'язковою")
+            .MustAsync(async (name, cancellation) =>
+                    !await db.Categories.AnyAsync(c => c.Name == name, cancellation))
+            .WithMessage("Категорія з такою назвою вже існує")
             .MaximumLength(250)
             .WithMessage("Назва повинна містити не більше 250 символів");
         RuleFor(x => x.Slug)
@@ -20,7 +25,7 @@ public class CategoryCreateValidator : AbstractValidator<CategoryCreateModel>
         RuleFor(x => x.ImageFile)
             //.NotNull().WithMessage("Файл зображення є обов'язковим")
             .NotEmpty().WithMessage("Файл зображення є обов'язковим");
-            //.Must(file => file!.Length > 0)
-            //.WithMessage("Файл не повинен бути порожнім");
+        //.Must(file => file!.Length > 0)
+        //.WithMessage("Файл не повинен бути порожнім");
     }
 }
--- my-pizushi/src/pages/categories/Create/index.js
+++ my-pizushi/src/pages/categories/Create/index.js
@@ -9,8 +9,8 @@ import * as Yup from "yup";
 import {useFormik} from "formik";
 
 const validationSchema = Yup.object().shape({
-    //name: Yup.string().required("Name is required"),
-    //slug: Yup.string().required("Slug is required"),
+    name: Yup.string().required("Вкажіть назву"),
+    slug: Yup.string().required("Вкажіть слаг"),
     imageFile: Yup.mixed().nullable()
 });
 
