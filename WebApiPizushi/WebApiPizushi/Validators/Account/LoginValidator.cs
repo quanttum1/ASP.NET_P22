0de78bb1be8b83e4200d46d0894bd9a6c6aca7ae
novakvova <novakvova@gmail.com>
2025-05-27 20:29:11 +0300

    Login Validation is good

---
+++ WebApiPizushi/WebApiPizushi/Validators/Account/LoginValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using WebApiPizushi.Data.Entities.Identity;
+using WebApiPizushi.Models.Account;
+
+namespace WebApiPizushi.Validators.Account;
+
+public class LoginValidator : AbstractValidator<LoginModel>
+{
+    public LoginValidator(UserManager<UserEntity> userManager)
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Електронна пошта є обов'язковою")
+            .EmailAddress().WithMessage("Некоректний формат електронної пошти")
+            .MustAsync(async (email, cancellation) =>
+            {
+                var user = await userManager.FindByEmailAsync(email);
+                return user != null;
+            }).WithMessage("Користувача з такою поштою не знайдено");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Пароль є обов'язковим")
+            .MinimumLength(6).WithMessage("Пароль повинен містити щонайменше 6 символів");
+
+        RuleFor(x => x)
+            .MustAsync(async (model, cancellation) =>
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+                if (user == null) return false;
+                return await userManager.CheckPasswordAsync(user, model.Password);
+            })
+            .WithMessage("Невірний email або пароль");
+    }
+}
